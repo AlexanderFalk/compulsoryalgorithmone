f225a6bc914f9fe5e5a3a0eb3f76ecc17efc93c1
Alexander Falk <alexfalk7@gmail.com>
2018-02-18 21:27:10 +0100

    Birthday problem updated and Coupon collector added

--- Birthday-problem.go
+++ Birthday-problem.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"os"
 	"time"
@@ -14,31 +15,44 @@ func dup_count(list []int) {
 		// check if the item/element exist in the duplicate_frequency map
 		_, exist := duplicate_frequency[item]
 		if exist {
-			duplicate_frequency[item] += 1 // increase counter by 1 if already in the map
 			fmt.Println("Duplicated value: ", item)
+			count := len(duplicate_frequency)
+			hypothesis(float64(count))
 			os.Exit(1)
+			duplicate_frequency[item] += 1 // increase counter by 1 if already in the map
+
 		} else {
 			duplicate_frequency[item] = 1 // else start counting from 1
 		}
 	}
+
 }
 
-func hypothesis(length int) bool {
+func hypothesis(length float64) bool {
+	result := (math.Pi * 50) / 2
+	hypoConf := math.Sqrt(result)
 
+	newLength := math.Floor(length)
+	newHypo := math.Floor(hypoConf)
+
+	if newLength == newHypo {
+		fmt.Printf("Hypothesis confirmed: %d = %d", newLength, newHypo)
+		return true
+	}
+	fmt.Printf("Hypothesis denied: %d = %d", newLength, newHypo)
+	return false
 }
 
 func Generate_Sequence(n int) {
 	var listOfNumbers []int
 	var index = 0
-	if len(listOfNumbers) == 0 {
-		fmt.Println("Error")
-	}
+
 	rand.Seed(time.Now().UTC().UnixNano())
 	for {
-		value := rand.Intn(n)
+		value := rand.Intn(n - 1)
 		fmt.Println(value)
 
-		fmt.Println(index)
+		//fmt.Println(index)
 		listOfNumbers = append(listOfNumbers, value)
 		index++
 		dup_count(listOfNumbers)
@@ -48,5 +62,7 @@ func Generate_Sequence(n int) {
 }
 
 func main() {
-	Generate_Sequence(100)
+
+	Generate_Sequence(50)
+
 }
--- coupon_collector.go
+++ coupon_collector.go
@@ -1,5 +1,33 @@
 package main
 
 import (
-	""
+	"fmt"
+	"math/rand"
+	"time"
 )
+
+func collect_coupons(n int) {
+	var isCollected = make([]bool, n)
+	var count int = 0
+	var distinct int = 0
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	for distinct < n {
+		value := rand.Intn(n)
+		//fmt.Println(value)
+		count++
+
+		//Every time a value doesn't match anything in the array, then we add it.
+		//When the distinct is bigger than n, then we stop the program and print the count
+		if !isCollected[value] {
+			distinct++
+			isCollected[value] = true
+		}
+	}
+
+	fmt.Println("Total collected coupons: ", count)
+}
+
+func main() {
+	collect_coupons(1000)
+}
